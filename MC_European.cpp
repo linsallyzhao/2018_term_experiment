f3aebfc7f6cb355374930f4eb309ff2bc0c3b98c
Lin Zhao <lin.sally.zhao@gmail.com>
2018-01-10 10:16:39 +0000

    Stopped using namespace std

--- MC_European.cpp
+++ MC_European.cpp
@@ -5,7 +5,6 @@
 #include <numeric>
 #include <algorithm>
 #include <chrono>
-using namespace std;
 
 // want to change these to const function. They don't modify things
 double one_step (double &old, double &inRate, double &vol, double &dt);
@@ -16,22 +15,22 @@ double end_price (int &steps, double &start, double &inRate, double &vol, double
 int main() {
     double inRate = 0.05, sigma = 0.2, expire = 1;
     double stock = 100, strike = 100;
-    int steps = 252; 
+    int steps = 252;
     double dt = expire / steps;
     double discount_factor = exp(-inRate * expire);
     int npaths = 10000;
-    vector<double> pay_offs;
+    std::vector<double> pay_offs;
     double payoff;
     for (int n = 1; n <= npaths; n++) {
-        payoff = max(end_price(steps, stock, inRate, sigma, dt) - strike, 0.0);
-//cout << "pay off " << payoff << endl;
+        payoff = std::max(end_price(steps, stock, inRate, sigma, dt) - strike, 0.0);
+//std::cout << "pay off " << payoff << std::endl;
         pay_offs.push_back(payoff);
     }
 
     double avg = accumulate(pay_offs.begin(), pay_offs.end(), 0.0) / pay_offs.size();
     double price = discount_factor * avg;
 
-    cout << "Monte carlo price is " << price << endl;
+    std::cout << "Monte carlo price is " << price << std::endl;
 
     return 0;
 }
@@ -40,13 +39,13 @@ int main() {
 double one_step (double &old, double &inRate, double &vol, double &dt){
 // the structure of the code, creat obj each time, is this too messy/expensive?
     //minstd_rand0 generator;
-    normal_distribution<double> norDist(0.0, 1.0);
+    std::normal_distribution<double> norDist(0.0, 1.0);
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    default_random_engine generator(seed);
+    std::default_random_engine generator(seed);
     double one_draw = norDist(generator);
-//cout << "One draw " << one_draw << endl;
+//std::cout << "One draw " << one_draw << std::endl;
     double new_price = old * (1 + inRate * dt + vol * sqrt(dt) * one_draw);
-//cout << "update price, new price is " << new_price << endl;
+//std::cout << "update price, new price is " << new_price << std::std::endl;
     return new_price;
 }
 
@@ -55,7 +54,7 @@ double end_price (int &steps, double &start, double &inRate, double &vol, double
     for (int n = 1; n <= steps; n++) {
         current_price = one_step(current_price, inRate, vol, dt);
     }
-//cout << "end price is " << current_price << endl;
+//std::cout << "end price is " << current_price << std::endl;
     return current_price;
 }
 
