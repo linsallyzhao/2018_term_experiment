97fa5377a72dcadea820595bc43b19bdb2eac8b2
Lin Zhao <lin.sally.zhao@gmail.com>
2018-01-14 19:46:26 +0000

    Create struct Option and add a break in the switch

--- MC_European.cpp
+++ MC_European.cpp
@@ -3,31 +3,42 @@
 #include <cmath>
 #include <chrono>
 
-double one_step (const double &, double &, double &, double &);
-double end_price (const int &, double &, double &, double &, double &);
-double max_price (const int &, double &, double &, double &, double &);
-double min_price (const int &, double &, double &, double &, double &);
-double get_payoff(int &, int &, double &, double &, double &, double &, double &);
-double diff_simu_min_price(const int &steps, double &start, double &inRate, double &vol, double &dt); 
+struct Option {
+    double inRate, vol, expire, stock, strike;
+    int steps;
+    double dt, discount_factor;
+};
+
+double get_payoff(const int &type, const Option &put);
+double one_step (const double &old, const Option &put);
+double diff_simu_min_price(const Option &put);
+double end_price (const Option &put);
+double min_price (const Option &put);
+double max_price (const Option &put);
+
 
 
 int main() {
 //swear I am going to put these into a strucure
-    double inRate = 0.1, sigma = 0.3, expire = 0.5;
-    double stock = 1, strike = 1;
-    int steps = 1;
-    double dt = expire / steps;
-    double discount_factor = exp(-inRate * expire);
+    Option put;
+    put.inRate = 0.1;
+    put.vol = 0.3;
+    put.expire = 0.5;
+    put.stock = 1;
+    put.strike = 1;
+    put.steps = 50;
+    put.dt = put.expire / put.steps;
+    put.discount_factor = exp(-put.inRate * put.expire);
     int npaths = 1000000;
     double running_avg = 0;
     double payoff;
     for (int type = 0; type <= 1; type++) {
         for (int n = 1; n <= npaths; n++) {
-            payoff = get_payoff(type, steps, stock, inRate, sigma, dt, strike);
+            payoff = get_payoff(type, put);
             running_avg = (running_avg * (n - 1) + payoff) / n;
         }
 
-        double price = discount_factor * running_avg;
+        double price = put.discount_factor * running_avg;
         if (type == 0 ) {
             std::cout << "Monte carlo price for lookback min_price put is " << price << std::endl;
         }
@@ -40,43 +51,44 @@ int main() {
 }
 
 
-double get_payoff(int &type, int &steps, double &stock, double &inRate,
-            double &sigma, double &dt, double &strike) {
+double get_payoff(const int &type, const Option &put) {
     double payoff;
 
     switch(type) {
-        case 0 : payoff = std::max(strike - min_price(steps, stock, inRate, sigma, dt), 0.0);
+        case 0 : payoff = std::max(put.strike - min_price(put), 0.0);
                  break;
 
-        case 1 : payoff = std::max(strike - diff_simu_min_price(steps, stock, inRate, sigma, dt), 0.0);
+        case 1 : payoff = std::max(put.strike - diff_simu_min_price(put), 0.0);
+                 break;
     }
 
     return payoff;
 }
 
-double one_step (const double &old, double &inRate, double &vol, double &dt){
-// the structure of the code, creat obj each time, is this too messy/expensive?
+double one_step (const double &old, const Option &put) {
     std::normal_distribution<double> norDist(0.0, 1.0);
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     double one_draw = norDist(generator);
-    double new_price = old * (1 + inRate * dt + vol * sqrt(dt) * one_draw);
+    double new_price =
+            old * (1 + put.inRate * put.dt + put.vol * sqrt(put.dt) * one_draw);
     return new_price;
 }
 
-double diff_simu_min_price(const int &steps, double &start, double &inRate,
-        double &vol, double &dt) {
+double diff_simu_min_price(const Option &put) {
     double minPrice = 100;
     double cum_lnS = 0;
-    double current_price = start;
+    double current_price = put.stock;
     std::normal_distribution<double> norDist(0.0, 1.0);
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    for (int count = 1; count <= steps;  count++) {
+    for (int count = 1; count <= put.steps;  count++) {
         double one_draw = norDist(generator);
-        double dln_S = (inRate - 0.5 * std::pow(vol, 2)) * dt + vol * sqrt(dt) * one_draw;
+        double dln_S =
+        (put.inRate - 0.5 * std::pow(put.vol, 2)) * put.dt
+        + put.vol * sqrt(put.dt) * one_draw;
         cum_lnS += dln_S;
-        current_price = start * exp(cum_lnS);
+        current_price = put.stock * exp(cum_lnS);
         if (current_price < minPrice) {
             minPrice = current_price;
         }
@@ -85,21 +97,21 @@ double diff_simu_min_price(const int &steps, double &start, double &inRate,
     return minPrice;
 }
 
-double end_price (const int &steps, double &start, double &inRate, double &vol, double &dt) {
-    double current_price = start;
-    for (int n = 1; n <= steps; n++) {
-        current_price = one_step(current_price, inRate, vol, dt);
+double end_price (const Option &put) {
+    double current_price = put.stock;
+    for (int n = 1; n <= put.steps; n++) {
+        current_price = one_step(current_price, put);
     }
     return current_price;
 }
 
 
 
-double max_price (const int &steps, double &start, double &inRate, double &vol, double &dt) {
+double max_price (const Option &put) {
     double maxPrice = -100;
-    double current_price = start;
-    for (int n = 1; n <= steps; n++) {
-        current_price = one_step(current_price, inRate, vol, dt);
+    double current_price = put.stock;
+    for (int n = 1; n <= put.steps; n++) {
+        current_price = one_step(current_price, put);
         if(current_price > maxPrice) {
             maxPrice = current_price;
         }
@@ -108,11 +120,11 @@ double max_price (const int &steps, double &start, double &inRate, double &vol, 
     return maxPrice;
 }
 
-double min_price (const int &steps, double &start, double &inRate, double &vol, double &dt) {
+double min_price (const Option &put) {
     double minPrice = 1000;
-    double current_price = start;
-    for (int n = 1; n <= steps; n++) {
-        current_price = one_step(current_price, inRate, vol, dt);
+    double current_price = put.stock;
+    for (int n = 1; n <= put.steps; n++) {
+        current_price = one_step(current_price, put);
         if(current_price < minPrice) {
             minPrice = current_price;
         }
