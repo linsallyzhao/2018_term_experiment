453ca3ab1676ad32802b327f25fc654e3a2273ef
Lin Zhao <lin.sally.zhao@gmail.com>
2018-01-14 19:57:32 +0000

    clang-format

--- MC_European.cpp
+++ MC_European.cpp
@@ -1,7 +1,7 @@
+#include <chrono>
+#include <cmath>
 #include <iostream>
 #include <random>
-#include <cmath>
-#include <chrono>
 
 struct Option {
     double inRate, vol, expire, stock, strike;
@@ -10,16 +10,14 @@ struct Option {
 };
 
 double get_payoff(const int &type, const Option &put);
-double one_step (const double &old, const Option &put);
+double one_step(const double &old, const Option &put);
 double diff_simu_min_price(const Option &put);
-double end_price (const Option &put);
-double min_price (const Option &put);
-double max_price (const Option &put);
-
-
+double end_price(const Option &put);
+double min_price(const Option &put);
+double max_price(const Option &put);
 
 int main() {
-//swear I am going to put these into a strucure
+    // swear I am going to put these into a strucure
     Option put;
     put.inRate = 0.1;
     put.vol = 0.3;
@@ -39,39 +37,41 @@ int main() {
         }
 
         double price = put.discount_factor * running_avg;
-        if (type == 0 ) {
-            std::cout << "Monte carlo price for lookback min_price put is " << price << std::endl;
-        }
-        else if (type == 1) {
-            std::cout << "Monte carlo price for lookback diff_simu put is " << price << std:: endl;
+        if (type == 0) {
+            std::cout << "Monte carlo price for lookback min_price put is "
+                      << price << std::endl;
+        } else if (type == 1) {
+            std::cout << "Monte carlo price for lookback diff_simu put is "
+                      << price << std::endl;
         }
     }
 
     return 0;
 }
 
-
 double get_payoff(const int &type, const Option &put) {
     double payoff;
 
-    switch(type) {
-        case 0 : payoff = std::max(put.strike - min_price(put), 0.0);
-                 break;
+    switch (type) {
+        case 0:
+            payoff = std::max(put.strike - min_price(put), 0.0);
+            break;
 
-        case 1 : payoff = std::max(put.strike - diff_simu_min_price(put), 0.0);
-                 break;
+        case 1:
+            payoff = std::max(put.strike - diff_simu_min_price(put), 0.0);
+            break;
     }
 
     return payoff;
 }
 
-double one_step (const double &old, const Option &put) {
+double one_step(const double &old, const Option &put) {
     std::normal_distribution<double> norDist(0.0, 1.0);
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     double one_draw = norDist(generator);
     double new_price =
-            old * (1 + put.inRate * put.dt + put.vol * sqrt(put.dt) * one_draw);
+        old * (1 + put.inRate * put.dt + put.vol * sqrt(put.dt) * one_draw);
     return new_price;
 }
 
@@ -82,11 +82,10 @@ double diff_simu_min_price(const Option &put) {
     std::normal_distribution<double> norDist(0.0, 1.0);
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    for (int count = 1; count <= put.steps;  count++) {
+    for (int count = 1; count <= put.steps; count++) {
         double one_draw = norDist(generator);
-        double dln_S =
-        (put.inRate - 0.5 * std::pow(put.vol, 2)) * put.dt
-        + put.vol * sqrt(put.dt) * one_draw;
+        double dln_S = (put.inRate - 0.5 * std::pow(put.vol, 2)) * put.dt +
+                       put.vol * sqrt(put.dt) * one_draw;
         cum_lnS += dln_S;
         current_price = put.stock * exp(cum_lnS);
         if (current_price < minPrice) {
@@ -97,7 +96,7 @@ double diff_simu_min_price(const Option &put) {
     return minPrice;
 }
 
-double end_price (const Option &put) {
+double end_price(const Option &put) {
     double current_price = put.stock;
     for (int n = 1; n <= put.steps; n++) {
         current_price = one_step(current_price, put);
@@ -105,14 +104,12 @@ double end_price (const Option &put) {
     return current_price;
 }
 
-
-
-double max_price (const Option &put) {
+double max_price(const Option &put) {
     double maxPrice = -100;
     double current_price = put.stock;
     for (int n = 1; n <= put.steps; n++) {
         current_price = one_step(current_price, put);
-        if(current_price > maxPrice) {
+        if (current_price > maxPrice) {
             maxPrice = current_price;
         }
     }
@@ -120,16 +117,15 @@ double max_price (const Option &put) {
     return maxPrice;
 }
 
-double min_price (const Option &put) {
+double min_price(const Option &put) {
     double minPrice = 1000;
     double current_price = put.stock;
     for (int n = 1; n <= put.steps; n++) {
         current_price = one_step(current_price, put);
-        if(current_price < minPrice) {
+        if (current_price < minPrice) {
             minPrice = current_price;
         }
     }
 
     return minPrice;
 }
-
