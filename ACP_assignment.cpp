ae376fac53529cf545794d5e69f49ed76ba146bf
Lin Zhao <lin.sally.zhao@gmail.com>
2018-04-07 21:46:35 +0000

    Make payoff generation one-time only

--- ACP_assignment.cpp
+++ ACP_assignment.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 #include <random>
 #include <vector>
-
+#include <unordered_map>
+#include <string>
 struct Option {
     double inRate, vol, expire, stock, strike;
     int steps;
     double dt, discount_factor;
 };
 
-double get_payoff(const int &gap, const bool &floating, const bool &call,
-            const bool &arithmetic, const bool &continous, const Option &option);
+std::unordered_map<std::string, double> get_payoffs(const int &gap, const Option &option);
 double one_step(const double &old, const Option &option);
 
 int main() {
@@ -24,92 +24,81 @@ int main() {
     option.steps = 252;
     option.dt = option.expire / option.steps;
     option.discount_factor = exp(-option.inRate * option.expire);
-    int npaths = 1000;
-    double running_avg = 0;
-    double payoff = -1;
-    std::vector<bool> argBool {true, false};
+    int npaths = 100000;
+    std::unordered_map<std::string, double> payoffs;
     std::vector<int> gaps {5, 22};
-    for (int gap : gaps) {
-        for (bool flo : argBool) {
-            for (bool op_type : argBool) {
-                for (bool ave_type : argBool) {
-                    running_avg = 0;
-                    for (int n = 1; n <= npaths; n++) {
-                        payoff = get_payoff(gap, flo, op_type, ave_type, false, option);
-                        running_avg = (running_avg * (n - 1) + payoff) / n;
-                    }
+    std::unordered_map<std::string, double> running_avgs;
+    int gap = 5;
 
-                    double price = option.discount_factor * running_avg;
-                    std::cout << "discrete_sampling_" << gap << "days ";
-                    std::cout << (flo ? "floating_strike " : "fixed_strike ");
-                    std::cout << (ave_type ? "arith_avg " : "geom_avg ");
-                    std::cout << (op_type ? "call_opt " : "put_opt ");
-                    std::cout << price << std::endl;
-                }
-            }
+    for (const auto & [ key, value ] : payoffs) {
+        running_avgs[key] = 0.0;
+    }
+
+    for (int n = 1; n <= npaths; n++) {
+        payoffs = get_payoffs(gap, option);
+        for (const auto & [ key, value ] : payoffs) {
+            running_avgs[key] = (running_avgs[key] * (n - 1) + value) / n;
         }
     }
 
-    for (bool flo : argBool) {
-        for (bool op_type : argBool) {
-            for (bool ave_type : argBool) {
-                running_avg = 0;
-                for (int n = 1; n <= npaths; n++) {
-                    payoff = get_payoff(5, flo, op_type, ave_type, true, option);
-                    running_avg = (running_avg * (n - 1) + payoff) / n;
-                }
-
-                double price = option.discount_factor * running_avg;
-                std::cout << "continuous_sampling ";
-                std::cout << (flo ? "floating_strike " : "fixed_strike ");
-                std::cout << (ave_type ? "arith_avg " : "geom_avg ");
-                std::cout << (op_type ? "call_opt " : "put_opt ");
-                std::cout << price << std::endl;
-            }
-        }
+    double price = 0.0;
+    for (const auto & [ key, value ] : running_avgs) {
+        price = option.discount_factor * value;
+        std::cout << key << " " << price << std::endl;
     }
+
 
     return 0;
 }
 
-double get_payoff(const int &gap, const bool &floating, const bool &call, const bool &arithmetic, const bool &continous, const Option &option) {
+
+std::unordered_map<std::string, double> get_payoffs(const int &gap, const Option &option) {
     double payoff = -1;
     double old_price = option.stock;
     double new_price = one_step(old_price, option);
-    double average = new_price;
+    std::unordered_map<std::string, double> averages = {
+        {"con_ari", new_price},
+        {"con_geo", new_price},
+        {"dis_ari", new_price},
+        {"dis_geo", new_price}
+    };
     double tmp;
     old_price = new_price;
     for (int i = 2; i <= option.steps; i++) {
         new_price = one_step(old_price, option);
-        if (continous){
-            if (arithmetic){
-                average = (average * (i - 1) + new_price) / i;
-            } else {
-                average = std::pow(average, (i - 1.0) / i) * std::pow(new_price, 1.0/i);
-            }
-        } else {
-            int count = 0;
-            if ((i - 1) % gap == 0) {
-                count = (i - 1) / gap;
-                if (arithmetic){
-                    average = (average * count + new_price) / (count + 1);
-                } else {
-                    average = std::pow((std::pow(average, count) * new_price), 1.0/(count + 1));
-                }
-            }
-        }
+        averages["con_ari"] = (averages["con_ari"] * (i - 1) + new_price) / i;
+        averages["con_geo"] = std::pow(averages["con_geo"], (i - 1.0) / i) * std::pow(new_price, 1.0/i);
 
+        int count = 0;
+        if ((i - 1) % gap == 0) {
+            count = (i - 1) / gap;
+            averages["dis_ari"] = (averages["dis_ari"] * count + new_price) / (count + 1);
+            averages["dis_geo"] = std::pow((std::pow(averages["dis_geo"], count) * new_price), 1.0/(count + 1));
+        }
         old_price = new_price;
     }
 
-    if (floating) {
-        tmp = call ? new_price - average : average - new_price;
-    } else {
-        tmp = call ? average - option.strike : option.strike - average;
-    }
-    payoff = std::max(tmp, 0.0);
+    std::unordered_map<std::string, double> payoffs;
+    payoffs["con_ari_flo_call"] = std::max(new_price - averages["con_ari"], 0.0);
+    payoffs["con_ari_flo_put"] = std::max(averages["con_ari"] - new_price, 0.0);
+    payoffs["con_ari_fix_call"] = std::max(averages["con_ari"] - option.strike, 0.0);
+    payoffs["con_ari_fix_put"] = std::max(option.strike -  averages["con_ari"], 0.0);
 
-    return payoff;
+    payoffs["con_geo_flo_call"] = std::max(new_price - averages["con_geo"], 0.0);
+    payoffs["con_geo_flo_put"] = std::max(averages["con_geo"] - new_price, 0.0);
+    payoffs["con_geo_fix_call"] = std::max(averages["con_geo"] - option.strike, 0.0);
+    payoffs["con_geo_fix_put"] = std::max(option.strike -  averages["con_geo"], 0.0);
+
+    payoffs["dis_ari_flo_call"] = std::max(new_price - averages["dis_ari"], 0.0);
+    payoffs["dis_ari_flo_put"] = std::max(averages["dis_ari"] - new_price, 0.0);
+    payoffs["dis_ari_fix_call"] = std::max(averages["dis_ari"] - option.strike, 0.0);
+    payoffs["dis_ari_fix_put"] = std::max(option.strike -  averages["dis_ari"], 0.0);
+
+    payoffs["dis_geo_flo_call"] = std::max(new_price - averages["dis_geo"], 0.0);
+    payoffs["dis_geo_flo_put"] = std::max(averages["dis_geo"] - new_price, 0.0);
+    payoffs["dis_geo_fix_call"] = std::max(averages["dis_geo"] - option.strike, 0.0);
+    payoffs["dis_geo_fix_put"] = std::max(option.strike -  averages["dis_geo"], 0.0);
+    return payoffs;
 }
 
 double one_step(const double &old, const Option &option) {
